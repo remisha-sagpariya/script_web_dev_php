43a47a33a7e79a86000bd0e5dd70ac425ca345b6
remisha-sagpariya <143687290+remisha-sagpariya@users.noreply.github.com>
2023-10-26 14:40:13 +0530

    Create a multidimensional array to store information about movies, including title, director, and release year for at least two movies. Display the information for one of the movies.

---
+++ Array_Exercise/script_15.php
@@ -0,0 +1,13 @@
+<?php
+
+$movies=array(array("title"=>"RRR","director"=>"rajamauli","releasedate"=>"2022"),
+			array("title"=>"Jawan","director"=>"atlee kumar","releasedate"=>"2023"));
+
+foreach ($movies as $movie)
+{
+	print_r ($movie);
+	echo "</br>";
+}
+
+
+?>
